3e61240d0fe189b3d553d8db7d0e6e84f6d7e706
nvdinh185 <nvdinh0766777123@gmail.com>
2021-05-21 14:34:07 +0700

    new react
    test
    
    ok
    
    ok
    
    data table ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './react/App';
+import ReactDOM from "react-dom";
+import data from "./movies";
+import DataTable from "react-data-table-component";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [hideDirector, setHideDirector] = React.useState(false);
+
+    const columns = React.useMemo(() => [
+        {
+            name: 'Title',
+            selector: 'title',
+            sortable: true,
+        },
+        {
+            name: 'Director',
+            selector: 'director',
+            sortable: true,
+            omit: hideDirector,
+        },
+        {
+            name: 'Year',
+            selector: 'year',
+            sortable: true,
+        },
+    ], [hideDirector]);
+
+    return (
+        <>
+            <button onClick={() => setHideDirector(!hideDirector)}>Hide Directory Column</button>
+            <DataTable
+                title="Movie List"
+                columns={columns}
+                data={data}
+            />
+        </>
+    );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ src/movies.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: "Beetlejuice",
+    director: "Beetlejuice",
+    year: "1988"
+  },
+  {
+    id: 2,
+    title: "The Cotton Club",
+    director: "The Cotton Club",
+    year: "1984",
+  },
+  {
+    id: 3,
+    title: "The Shawshank Redemption",
+    director: "The Shawshank Redemption",
+    year: "1994",
+  },
+];
